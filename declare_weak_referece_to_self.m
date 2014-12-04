9fa9367a96624b4c5c88f6a459f82b4e0b81d5b0
eelcokoelewijn-afrogleap <eelcokoelewijn@Eelcos-MacBook-Pro-2.local>
2014-12-04 15:47:08 +0100

    Weak self code snippet

---
+++ declare_weak_referece_to_self.m
@@ -0,0 +1,4 @@
+// IDECodeSnippetTitle: Declare weak referece to self
+// IDECodeSnippetCompletionScopes: CodeBlock
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+__weak typeof(self)weakSelf = self;
