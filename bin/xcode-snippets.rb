ac90a14b66ec754412f35d5c742bb948301563e0
eelcokoelewijn-afrogleap <eelcokoelewijn@Eelcos-MacBook-Pro-2.local>
2014-12-04 16:50:22 +0100

    Update script to extract and add snippets based on argument: extract or add. Add is not implemented.

--- bin/xcode-snippets.rb
+++ bin/xcode-snippets.rb
@@ -1,6 +1,12 @@
-#!/usr/bin/ruby -w
+#!/usr/bin/ruby
 require 'plist' # https://github.com/bleything/plist
 require 'rexml/document'
+
+# helper methods
+# print message to console
+def outputLogMessage(message)
+  puts message
+end
 
 # // Set Frame
 # // Initializes a view frame inside a code block
@@ -9,43 +15,54 @@ require 'rexml/document'
 # // Completion Scope: Function or Method
 # // [Codesnippet]
 
+startOptions = ARGV[0]
+
 wkDir = Dir.getwd
+xcodeDir = Dir.home+"/Library/Developer/Xcode/UserData/CodeSnippets"
 
-Dir.chdir(Dir.home+"/Library/Developer/Xcode/UserData/CodeSnippets")
+if startOptions == "extract"
+  outputLogMessage("Extracting snippets from #{xcodeDir}")
 
-codeSnippetCompletionScopes = ""
-codeSnippetContents = ""
-codeSnippetIdentifier = ""
-codeSnippetLanguage = ""
-codeSnippetTitle = ""
-codeSnippetUserSnippet = ""
-codeSnippetVersion = ""
+  Dir.chdir(xcodeDir)
 
-Dir.glob("*.codesnippet") do |snippetFilename|
-  puts snippetFilename
+  codeSnippetCompletionScopes = ""
+  codeSnippetContents = ""
+  codeSnippetIdentifier = ""
+  codeSnippetLanguage = ""
+  codeSnippetTitle = ""
+  codeSnippetUserSnippet = ""
+  codeSnippetVersion = ""
 
-  result = Plist::parse_xml(snippetFilename)
+  Dir.glob("*.codesnippet") do |snippetFilename|
+    # outputLogMessage(snippetFilename)
 
-  # key : IDECodeSnippetCompletionScopes
-  codeSnippetCompletionScopes = result['IDECodeSnippetCompletionScopes'][0]
-  # key : IDECodeSnippetContents
-  codeSnippetContents = result['IDECodeSnippetContents']
-  # key : IDECodeSnippetIdentifier
-  codeSnippetIdentifier = result['IDECodeSnippetIdentifier']
-  # key : IDECodeSnippetLanguage
-  codeSnippetLanguage = result['IDECodeSnippetLanguage']
-  # key : IDECodeSnippetTitle
-  codeSnippetTitle = result['IDECodeSnippetTitle']
-  # key : IDECodeSnippetUserSnippet
-  codeSnippetUserSnippet = result['IDECodeSnippetUserSnippet']
-  # key : IDECodeSnippetVersion
-  codeSnippetVersion = result['IDECodeSnippetVersion']
+    result = Plist::parse_xml(snippetFilename)
 
-  filename = codeSnippetTitle.downcase.strip.gsub(/[^\w\.\-]/,"_")
-  f = File.open(wkDir+"/"+filename.insert(-1,".m"),"w+")
-  f.puts("// IDECodeSnippetTitle: #{codeSnippetTitle}")
-  f.puts("// IDECodeSnippetCompletionScopes: #{codeSnippetCompletionScopes}")
-  f.puts("// IDECodeSnippetLanguage: #{codeSnippetLanguage}")
-  f.puts("#{codeSnippetContents}")
+    # key : IDECodeSnippetCompletionScopes
+    codeSnippetCompletionScopes = result['IDECodeSnippetCompletionScopes'][0]
+    # key : IDECodeSnippetContents
+    codeSnippetContents = result['IDECodeSnippetContents']
+    # key : IDECodeSnippetIdentifier
+    codeSnippetIdentifier = result['IDECodeSnippetIdentifier']
+    # key : IDECodeSnippetLanguage
+    codeSnippetLanguage = result['IDECodeSnippetLanguage']
+    # key : IDECodeSnippetTitle
+    codeSnippetTitle = result['IDECodeSnippetTitle']
+    # key : IDECodeSnippetUserSnippet
+    codeSnippetUserSnippet = result['IDECodeSnippetUserSnippet']
+    # key : IDECodeSnippetVersion
+    codeSnippetVersion = result['IDECodeSnippetVersion']
 
+    filename = codeSnippetTitle.downcase.strip.gsub(/[^\w\.\-]/,"_")
+    f = File.open(wkDir+"/"+filename.insert(-1,".m"),"w+")
+    f.puts("// IDECodeSnippetTitle: #{codeSnippetTitle}")
+    f.puts("// IDECodeSnippetCompletionScopes: #{codeSnippetCompletionScopes}")
+    f.puts("// IDECodeSnippetLanguage: #{codeSnippetLanguage}")
+    f.puts("#{codeSnippetContents}")
+
+  end
+elsif startOptions == "add"
+  outputLogMessage("Add snippets to #{xcodeDir}")
+else
+  outputLogMessage("Use xcode-snippets with 'extract' or 'add' argument")
 end
