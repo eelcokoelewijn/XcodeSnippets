0c4991a20b243b83d79da99c0916c81553f83e85
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2015-01-03 18:31:45 +0100

    Refactor code and completed the add feature. Tool now extracts and adds code snippets.

--- bin/xcode-snippets.rb
+++ bin/xcode-snippets.rb
@@ -1,12 +1,9 @@
 #!/usr/bin/ruby
 require 'plist' # https://github.com/bleything/plist
 require 'rexml/document'
-
-# helper methods
-# print message to console
-def outputLogMessage(message)
-  puts message
-end
+require_relative('../src/Logger.rb')
+require_relative('../src/Parser.rb')
+require_relative('../src/Writer.rb')
 
 # // Set Frame
 # // Initializes a view frame inside a code block
@@ -21,79 +18,23 @@ wkDir = Dir.getwd
 xcodeDir = Dir.home+"/Library/Developer/Xcode/UserData/CodeSnippets"
 
 if startOptions == "extract"
-  outputLogMessage("Extracting snippets from #{xcodeDir}")
+  Logger.show("Extracting snippets from #{xcodeDir}")
 
   Dir.chdir(xcodeDir)
 
-  codeSnippetCompletionScopes = ""
-  codeSnippetContents = ""
-  codeSnippetIdentifier = ""
-  codeSnippetLanguage = ""
-  codeSnippetTitle = ""
-  codeSnippetUserSnippet = ""
-  codeSnippetVersion = ""
-
   Dir.glob("*.codesnippet") do |snippetFilename|
-    # outputLogMessage(snippetFilename)
-
-    result = Plist::parse_xml(snippetFilename)
-
-    # key : IDECodeSnippetCompletionScopes
-    codeSnippetCompletionScopes = result['IDECodeSnippetCompletionScopes'][0]
-    # key : IDECodeSnippetContents
-    codeSnippetContents = result['IDECodeSnippetContents']
-    # key : IDECodeSnippetIdentifier
-    codeSnippetIdentifier = result['IDECodeSnippetIdentifier']
-    # key : IDECodeSnippetLanguage
-    codeSnippetLanguage = result['IDECodeSnippetLanguage']
-    # key : IDECodeSnippetTitle
-    codeSnippetTitle = result['IDECodeSnippetTitle']
-    # key : IDECodeSnippetUserSnippet
-    codeSnippetUserSnippet = result['IDECodeSnippetUserSnippet']
-    # key : IDECodeSnippetVersion
-    codeSnippetVersion = result['IDECodeSnippetVersion']
-
-    filename = codeSnippetTitle.downcase.strip.gsub(/[^\w\.\-]/,"_")
-    f = File.open(wkDir+"/"+filename.insert(-1,".m"),"w+")
-    f.puts("// IDECodeSnippetTitle: #{codeSnippetTitle}")
-    f.puts("// IDECodeSnippetCompletionScopes: #{codeSnippetCompletionScopes}")
-    f.puts("// IDECodeSnippetLanguage: #{codeSnippetLanguage}")
-    f.puts("#{codeSnippetContents}")
-
+    resultHash = Parser.plistFile(snippetFilename)
+    Writer.fileContents(resultHash, wkDir)
   end
+
 elsif startOptions == "add"
-  outputLogMessage("Add snippets to #{xcodeDir}")
+  Logger.show("Add snippets to #{xcodeDir}")
   Dir.glob("*.m") do |snippetFilename|
 
-    f = File.open(snippetFilename)
-    codesnippetPlist = Hash.new()
-    lines = f.readlines
-
-    codeSnippetTitle = lines[0].split(":")
-    codeSnippetTitleKey = codeSnippetTitle[0].sub("// ","")
-    codeSnippetTitleValue = codeSnippetTitle[1]
-    codesnippetPlist[codeSnippetTitleKey] = codeSnippetTitleValue
-
-    codeSnippetCompletionScopes = lines[1].split(":")
-    codeSnippetCompletionScopesKey = codeSnippetCompletionScopes[0].sub("// ","")
-    codeSnippetCompletionScopesValue = codeSnippetCompletionScopes[1]
-    codesnippetPlist[codeSnippetCompletionScopesKey] = [codeSnippetCompletionScopesValue]
-
-    codeSnippetLanguage = lines[2].split(":")
-    codeSnippetLanguageKey = codeSnippetLanguage[0].sub("// ","")
-    codeSnippetLanguageValue = codeSnippetLanguage[1]
-    codesnippetPlist[codeSnippetLanguageKey] = codeSnippetLanguageValue
-
-    # IDECodeSnippetIdentifier
-    # IDECodeSnippetVersion
-    codesnippetPlist["IDECodeSnippetVersion"] = 2
-    # IDECodeSnippetUserSnippet
-    codesnippetPlist["IDECodeSnippetUserSnippet"] = true
-    # IDECodeSnippetContents
-
-    puts codesnippetPlist.to_plist
+    resultHash = Parser.dotMFile(snippetFilename)
+    Writer.plistFile(resultHash, xcodeDir)
   end
 
 else
-  outputLogMessage("Use xcode-snippets with 'extract' or 'add' argument")
+  Logger.show("Use xcode-snippets with 'extract' or 'add' argument")
 end
---
+++ src/Writer.rb
@@ -0,0 +1,19 @@
+class Writer
+
+  # class method
+  def self.fileContents(contentHash, folder)
+    filename = contentHash['codeSnippetTitle'].downcase.strip.gsub(/[^\w\.\-]/,"_")
+    f = File.open(folder+"/"+filename.insert(-1,".m"),"w+")
+    f.puts("// IDECodeSnippetTitle: #{contentHash['codeSnippetTitle']}")
+    f.puts("// IDECodeSnippetCompletionScopes: #{contentHash['codeSnippetCompletionScopes']}")
+    f.puts("// IDECodeSnippetLanguage: #{contentHash['codeSnippetLanguage']}")
+    f.puts("#{contentHash['codeSnippetContents']}")
+  end
+
+  def self.plistFile(contentHash, folder)
+    filename = folder + "/" + contentHash['IDECodeSnippetIdentifier'] + ".codesnippet"
+    contentHash.save_plist(filename)
+    # puts contentHash.to_plist()
+  end
+
+end
