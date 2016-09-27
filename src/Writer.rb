9be3254b657a1792719522444c5676e2f8a5280a
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2016-09-27 16:24:45 +0200

    When extracting snippets distinguish between Obj-c and Swift files

--- src/Writer.rb
+++ src/Writer.rb
@@ -3,12 +3,21 @@ class Writer
   # class method
   def self.fileContents(contentHash, folder)
     filename = contentHash['codeSnippetTitle'].downcase.strip.gsub(/[^\w\.\-]/,"_")
-    f = File.open(folder+"/"+filename.insert(-1,".m"),"w+")
+    fileExtension = Writer.fileExtension(contentHash['codeSnippetLanguage'])
+    f = File.open(folder+"/"+filename.insert(-1,"#{fileExtension}"),"w+")
     f.puts("// IDECodeSnippetTitle: #{contentHash['codeSnippetTitle']}")
-    f.puts("// IDECodeSnippetCompletionPrefix: #{contentHash['codeSnippetCompletionPrefix']}")    
+    f.puts("// IDECodeSnippetCompletionPrefix: #{contentHash['codeSnippetCompletionPrefix']}")
     f.puts("// IDECodeSnippetCompletionScopes: #{contentHash['codeSnippetCompletionScopes']}")
     f.puts("// IDECodeSnippetLanguage: #{contentHash['codeSnippetLanguage']}")
     f.puts("#{contentHash['codeSnippetContents']}")
+  end
+
+  def self.fileExtension(snippetLanguage)
+    if snippetLanguage == "Xcode.SourceCodeLanguage.Swift"
+      return ".swift"
+    else
+      return ".m"
+    end
   end
 
   def self.plistFile(contentHash, folder)
