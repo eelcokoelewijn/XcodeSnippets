f0e5538cb6259895cd9a8001ce32b29c37df476f
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2017-04-07 07:53:22 +0200

    Add MVP template, Protocol with Self requirement, ViewModel template

---
+++ protocol_extension_with_self_requirement.swift
@@ -0,0 +1,14 @@
+// IDECodeSnippetTitle: Protocol extension with Self requirement
+// IDECodeSnippetCompletionPrefix: swift_protocol-extension-self
+// IDECodeSnippetCompletionScopes: TopLevel
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetSummary: Create protocol with default implementation based on Self requirement
+protocol <#protocol-name#>: class {
+    func <#function-name#>()
+}
+
+extension <#protocol-name#> where Self: <#type-name#> {
+    func <#function-name#>() {
+
+    }
+}
