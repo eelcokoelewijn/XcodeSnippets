3ee258f740900360dcc04ec92ac3603eb91f7e81
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2017-06-20 22:40:23 +0200

    Update snippet mixin-cake pattern with MVP to include view implementation

--- uses___mixin_cake_pattern_with_view_presenter.swift
+++ uses___mixin_cake_pattern_with_view_presenter.swift
@@ -4,27 +4,45 @@
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
 // IDECodeSnippetSummary: Uses & Mixin cake pattern DI with View Presenter
 protocol <#protocol-view-name#>View: class {
-		
-		}
-		
-		protocol <#protocol-presenter-name#>Presenter {
-		    func start()
-		    func setView(_ view: <#protocol-view-name#>View)
-		}
-		
-		protocol Uses<#protocol-presenter-name#>Presenter {
-		    var <#protocol-instance-presenter-name#>: <#protocol-presenter-name#>Presenter { get }
-		}
-		
-		class Mixin<#protocol-presenter-name#>Presenter: <#protocol-presenter-name#>Presenter {
-			weak var view: <#ViewName#>View?
-	        func start() {
-		
-			}
-		
-			func setView(_ view: <#protocol-view-name#>View) {
-	            self.view = view
-			}
-		}
-		
-	
+
+}
+
+protocol <#protocol-presenter-name#>Presenter {
+    func start()
+    func setView(_ view: <#protocol-view-name#>View)
+}
+
+protocol Uses<#protocol-presenter-name#>Presenter {
+    var <#name#>Presenter: <#protocol-presenter-name#>Presenter { get }
+}
+
+class Mixin<#protocol-presenter-name#>Presenter: <#protocol-presenter-name#>Presenter {
+	weak var view: <#ViewName#>View?
+    func start() {
+
+	}
+
+	func setView(_ view: <#protocol-view-name#>View) {
+        self.view = view
+	}
+}
+
+final <#ViewName#>ViewController: UIViewController, <#ViewName#>View, Uses<#protocol-presenter-name#>Presenter {
+	let <#name#>Presenter: <#protocol-presenter-name#>Presenter
+	private lazy var moduleView: <#name#>ModuleView = <#name#>ModuleView()
+
+	init(presenter: <#protocol-presenter-name#>Presenter) {
+		<#name#>Presenter = presenter
+		super.init(nibName: nil, bundle: nil)
+        <#name#>Presenter.setView(self)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        super.loadView()
+        view = moduleView
+    }
+}
