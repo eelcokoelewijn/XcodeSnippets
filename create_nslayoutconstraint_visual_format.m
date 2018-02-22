b717a943cea8514321cc3d25892b94a76222c24a
Eelco Koelewijn <eelco@eelcokoelewijn.nl>
2018-02-22 19:52:42 +0100

    New snippets for common swiftlint warnings, module and assembly creation, extensions for uiimage and uibutton for creating object based on color.

---
+++ create_nslayoutconstraint_visual_format.m
@@ -0,0 +1,7 @@
+// IDECodeSnippetTitle: Create NSLayoutConstraint visual format
+// IDECodeSnippetCompletionPrefix: cnstrnt
+// IDECodeSnippetCompletionScopes: CodeBlock
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetSummary: Create layout constraint using visual format
+ [<#view#> addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:<#visualformat#> options:0 metrics:<#metrics#> views:<#views#>]];
+	
